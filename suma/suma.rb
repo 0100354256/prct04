3c644ae2627678b078353be4d019ef9c0ba15ac0
Sergio Gonzalez <alu0100354256@ull.edu.es>
2013-10-07 20:11:32 +0100

    Editar suma

--- suma/suma.rb
+++ suma/suma.rb
@@ -1,21 +1,12 @@
 #! /usr/bin/ruby
-<<<<<<< HEAD
-#Suma de matrices
-
-#Declaración de variables
-=======
 # Suma de matrices
 
 # Declaración de variables
->>>>>>> suma
 a = [[3, 3, 2], [1, 2, 0], [1, 0, 2]] #Matriz a
 b = [[0, 1, 5], [6, 5, 0], [2, 1, 4]] #Matriz b
 
 c = Array.new #Matriz c (Resultado de la Suma)
 
-<<<<<<< HEAD
-m, n = a.size, a[1].size
-=======
 m, n = a.size, a[1].size
 
 # Inicialización Matriz de Suma
@@ -40,4 +31,3 @@ end
 c.each do |fila|
   puts fila.join(" ")
 end
->>>>>>> suma
