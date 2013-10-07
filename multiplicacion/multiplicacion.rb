3aaa3b268da01630411278a5a9f7eefc5166afca
Romina Martin <alu0100696776@ull.edu.es>
2013-10-07 18:48:30 +0100

    Declaracion Multiplicacion

---
+++ multiplicacion/multiplicacion.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/ruby
+
+# Declaración de variables
+
+a = [[1, 3, 2], [1, 0, 0]] 	#matriz a
+b = [[0, 0], [7, 5], [2, 1]]	#matriz b
+
+
+c = Array.new 	# matriz c -> Multiplicación
+
+ma, na = a.size, a[1].size
+mb, nb = b.size, b[1].size
