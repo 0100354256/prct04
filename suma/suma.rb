7edba3d00a128c32520c58da16e78274b0bb12bf
Romina Martin <alu0100696776@ull.edu.es>
2013-10-07 19:10:19 +0100

    Inicializacion Suma

--- suma/suma.rb
+++ suma/suma.rb
@@ -8,3 +8,13 @@ b = [[0, 1, 5], [6, 5, 0], [2, 1, 4]] #Matriz b
 c = Array.new #Matriz c (Resultado de la Suma)
 
 m, n = a.size, a[1].size
+
+#Inicialización Matriz de Suma
+
+for i in 0...m do
+  c[i] = Array.new
+
+  for j in 0...n do
+    c[i][j] = 0
+  end
+end
